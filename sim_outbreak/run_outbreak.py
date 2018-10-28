f647f6553f7a7a089178cfe41c5826157b854e4f
Priyanka <ppsekhar@gmail.com>
2018-10-28 14:36:30 -0700

    initial commit for outbreak simulation runs

---
+++ sim_outbreak/run_outbreak.py
@@ -0,0 +1,13 @@
+import snap
+
+def runOutBreakSteadyState():
+	print 'running outbreak with steady state'
+
+def runOutBreakTimestamp():
+	print 'running according to timestamps'
+
+def loadNetwork():
+	print 'Loading DNC Network'
+
+if __name__== "__main__":
+	print 'Running Outbreak with Timestamps'
